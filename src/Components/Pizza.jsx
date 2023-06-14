21d458104948a3e85ac092e39021d25134ad6d95
Niyas Kandengil <52659398+niyaskandengil@users.noreply.github.com>
2023-06-14 23:57:48 +0530

    #

--- src/Components/Pizza.jsx
+++ src/Components/Pizza.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import style from './salad.module.css';
 
 const Pizza = () => {
   return (
-    <div>Pizzaaaaaaaaaaaaaaaaa  Veno??</div>
+    <div className={style.pic}>
+        <img src="https://images8.alphacoders.com/644/644361.jpg" alt="1122"/>
+    </div>
   )
 }
 
